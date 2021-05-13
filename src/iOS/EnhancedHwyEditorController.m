f3fd60fabca5d0872462c8313039eb3f3bbfd15b
tannerwuster <tannerwuster@gmail.com>
2021-05-13 13:22:29 -0600

    deleted non-needed comments

--- src/iOS/EnhancedHwyEditorController.m
+++ src/iOS/EnhancedHwyEditorController.m
@@ -133,11 +133,7 @@ typedef enum {
     
     [self setOnewayBtnStyle];
     
-    //lanecount for stepper
-    // If there is a lanetag set that equal to the textValue and stepper value
-    // Else if empty we need to set it equal to zero
-    // current lane count
-    //    _laneCount = [[_keyValueDict valueForKey:@"lanes"] intValue];
+    // lanecount for stepper
     if([[_keyValueDict valueForKey:@"lanes"] intValue]) {
         laneStepper.value = [[_keyValueDict valueForKey:@"lanes"] intValue];
     } else {
@@ -389,7 +385,6 @@ typedef enum {
 
 - (IBAction)laneStepperPressed:(UIStepper *)sender {
     self.txtValue.text = [NSString stringWithFormat:@"%d", (int)laneStepper.value];
-    NSLog(@"Lanstepper line 370: %d", (int)laneStepper.value);
     NSMutableArray * laneTag;
     NSInteger * index = 0;
     for ( NSMutableArray * kv in _tags ){
